6b07bd59ef5e7f34354319b3bd66f2eae2d63f67
kostiasavva <k.savva@mail.ru>
2024-12-23 10:33:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Арифметика
+print('1st program')
+print(9 ** 0.5 * 5)
+
+# Логика
+print('2nd program')
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# Школьная загадка
+print('3rd program')
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+# Первый после точки
+print('4th program')
+print(int(float('123.456') * 10) % 10)
+
